d7bfaffede310a7adbafa5f47897b84098dd6d2e
Ana Ludmila <kn.ludi@gmail.com>
2022-07-13 09:41:08 -0300

    @knludi - Including ExerciseDataSource.kt

---
+++ app/src/main/java/com/example/myworkout/data/ExerciseDataSource.kt
@@ -0,0 +1,38 @@
+package com.example.myworkout.data
+
+import com.example.myworkout.R
+import com.example.myworkout.model.Exercise
+
+object ExerciseDataSource {
+    val Exercises: List<Exercise> = listOf(
+        Exercise(
+            R.drawable.abdominal,
+            "Abdominal",
+            "3",
+            "12",
+            "60''"
+        ),
+        Exercise(
+            R.drawable.agachamento_livre,
+            "Agachamento Livre",
+            "3",
+            "12",
+            "60''"
+        ),
+        Exercise(
+            R.drawable.esteira,
+            "Esteira",
+            "1",
+            "15 minutos",
+            ""
+        ),
+        Exercise(
+            R.drawable.panturrilha_unilateral,
+            "Panturrilha Unilateral",
+            "4",
+            "10",
+            "60''"
+        )
+
+    )
+}
