455dfde60acb17add212b7006cb8bd3e01ed1b9f
Ana Ludmila <kn.ludi@gmail.com>
2022-07-14 08:50:28 -0300

    @knludi - Implementing MainActivity.kt

--- app/src/main/java/com/example/myworkout/MainActivity.kt
+++ app/src/main/java/com/example/myworkout/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.myworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myworkout.adapter.ExerciseAdapter
+import com.example.myworkout.data.ExerciseDataSource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val dataSet = ExerciseDataSource.exercises
+
+        val recyclerView = findViewById<RecyclerView>(R.id.exercise_list_recycler_view)
+        recyclerView.adapter = ExerciseAdapter(dataSet)
+
+        recyclerView.setHasFixedSize(true)
     }
 }
